6db30150d01b3cf9dff3838061782c3fc07746c1
gomisha <misha@gomisha.com>
2018-06-21 08:51:42 -0400

    added generation of genesis block and new block

--- src/block.ts
+++ src/block.ts
@@ -1,21 +1,42 @@
+import SHA256 = require("crypto-js/sha256");
+
 export class Block {
-	timestamp: string;
+	timestamp: number;
 	lastHash: string;
 	hash: string;
 	data: string;
 
-	constructor(timestamp: string, lastHash: string, hash: string, data: string) {
+	constructor(timestamp: number, lastHash: string, hash: string, data: string) {
 		this.timestamp = timestamp;
 		this.lastHash = lastHash;
 		this.hash = hash;
 		this.data = data;
 	}
-	
-	static genesis() {
-		return new this('Genesis Time', '-----', 'f1r5t-ha4h', '');
+
+	/**
+	 * First block of the blockchain.
+	 */
+	static getGenesisBlock(): Block {
+		return new this(0, '-----', 'f1r5t-ha4h', '');
 	}
 
-	toString() {
+	/**
+	 * Mines new block that will be added to the blockchain.
+	 * @param lastBlock Link to the previous block for storing its hash
+	 * @param data Data to store of the new block
+	 */
+	static mineNewBlock(lastBlock: Block, data: string): Block {
+		const timestamp: number = Date.now();
+		const lastHash: string = lastBlock.hash;
+		const hash:string  = Block.generateHash(timestamp, lastHash, data);
+		return new this(timestamp, lastHash, hash, data)
+	}
+
+	static generateHash(timestamp: number, lastHash: string, data: string): string {
+		return SHA256(`${timestamp}${lastHash}${data}`).toString();
+	}
+
+	toString(): string {
 		return `Block:
 			Timestamp: ${this.timestamp}
 			Last Hash: ${this.lastHash.substring(0,10)}
