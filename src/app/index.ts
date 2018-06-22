897c89d24579ab7ae7f642202a0c99a71f81338c
gomisha <misha@gomisha.com>
2018-06-22 08:18:10 -0400

    installed body-parser middleware, added new "/mine" endpoint

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,14 +1,26 @@
 import express = require("express");
+import bodyParser = require("body-parser");
 
 import { Blockchain } from "../blockchain";
+import { request } from "http";
 
 const HTTP_PORT: string = process.env.HTTP_PORT || "3001";
 
 const app = express();
 const blockchain: Blockchain = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get("/blocks", (request, response) => {
     response.json(blockchain.chain);
+});
+
+app.post("/mine", (request, response) => {
+    const block = blockchain.addBlock(request.body.data)
+    console.log("New block added: " + block.toString());
+
+    //show updated chain with new block
+    response.redirect("/blocks");
 });
 
 app.listen(HTTP_PORT, () => {
